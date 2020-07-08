929fb9a12401cc509cb8637ce68a4082e3877c99
rijuvijayan <rijuvijayan@gmail.com>
2020-07-08 21:55:25 +1200

    user post and get route completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('./db/mongoose');
+const mongoose = require('mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
 
@@ -13,10 +14,50 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: 'Server Error!',
+        error: error,
+      });
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: 'Server Error!',
+        error: error,
+      });
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(400).send({
+      message: 'Invalid Object ID',
+    });
+  }
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message: 'Record not found',
+        });
+      }
       res.send(user);
     })
     .catch((error) => {
-      res.status(400).send(error);
+      res.status(500).send({
+        message: 'Server Error!',
+        error: error,
+      });
     });
 });
 
@@ -25,10 +66,13 @@ app.post('/tasks', (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((error) => {
-      res.status(400).send(error);
+      res.status(500).send({
+        message: 'Server Error!',
+        error: error,
+      });
     });
 });
 
