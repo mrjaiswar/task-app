29324f8a2fbab2529513c50c261f7c0a0b7fc38f
rijuvijayan <rijuvijayan@gmail.com>
2020-07-10 22:59:33 +1200

    jwt implemented on login and signup

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,6 +7,7 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
+    const token = await user.generateAuthToken();
     res.status(201).send(user);
   } catch (error) {
     res.status(500).send({
@@ -22,6 +23,7 @@ router.post('/users/login', async (req, res) => {
       req.body.email,
       req.body.password
     );
+    const token = await user.generateAuthToken();
     res.send(user);
   } catch (error) {
     res.status(400).send({
