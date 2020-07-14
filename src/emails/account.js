9eb817771da942cf23316b734b660c1c3aa06e76
rijuvijayan <rijuvijayan@gmail.com>
2020-07-14 14:26:44 +1200

    environment setup done

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,9 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendgridAPIKey =
-  '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';
-
-sgMail.setApiKey(sendgridAPIKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = (name, email) => {
   const msg = {
